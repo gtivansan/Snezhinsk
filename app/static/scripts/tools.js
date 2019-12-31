214993790cbac7bd9b05359d54f20b61f79f3970
Ivan Gaidai-Turlov <gtivansan@gmail.com>
2019-12-31 12:18:30 +0300

    added bezier tool

--- app/static/scripts/tools.js
+++ app/static/scripts/tools.js
@@ -29,7 +29,37 @@ function getSize(obj) {
     return {
         width: box.width,
         height: box.height
+    };
+}
+
+function complexMul(a, b) {
+    return {
+        x: a.x * b.x - a.y * b.y,
+        y: a.x * b.y + a.y * b.x
+    };
+}
+
+function complexSub(a, b) {
+    return {
+        x: a.x - b.x,
+        y: a.y - b.y
+    };
+}
+
+function complexAdd(a, b) {
+    return {
+        x: a.x + b.x,
+        y: a.y + b.y
+    };
+}
+
+function complexDiv(a, b) {
+    let bNorm = (b.x * b.x + b.y * b.y);
+    let _b = {
+        x: b.x / bNorm,
+        y: - b.y / bNorm
     }
+    return complexMul(a, _b);
 }
 
 class DraggablePoint {
@@ -63,6 +93,8 @@ class DraggablePoint {
             bubbles: true,
             detail: this.position
         }));
+
+        return false;
     }
 
     mouseup(_this, event, mousemoveHandler) {
@@ -116,19 +148,18 @@ class ToolChain {
         this.masksContainer = masksContainer;
         this.id = id;
 
-        this.isDrag = false;
         this.isActive = true;
-        this.r = 10
+        this.r = 10;
         this.points = [];
 
         this.shells = {};
         this.shells.root = addElement(this.canvas, "g", {id: `root_${this.id}`});
-        this.shells.points = addElement(this.shells.root, "g", {id: `points_${this.id}`});
         this.shells.segments = addElement(this.shells.root, "g", {id: `segments_${this.id}`});
+        this.shells.points = addElement(this.shells.root, "g", {id: `points_${this.id}`});
 
         let _this = this;
         this.mousedownHandler = (event) => {
-            if (!_this.isDrag && event.which == 1 && _this.isActive) {
+            if (event.target.tagName != "circle" && event.which == 1 && _this.isActive) {
                 _this.addPoint({
                     x: event.pageX - getCoords(_this.canvas).x, 
                     y: event.pageY - getCoords(_this.canvas).y
@@ -151,8 +182,6 @@ class ToolChain {
     addPoint(position) {
         position = this.positionModifier()(position);
         let point = new DraggablePoint(this.canvas, this.shells.points, position, {r: this.r}, {fill: "grey"})
-        point.node.addEventListener("draggablepoint-dragstart", (event) => this.isDrag = 1);
-        point.node.addEventListener("draggablepoint-dragend", (event) => this.isDrag = 0);
         point.positionModifier = this.positionModifier();
 
         this.points.push(point);
@@ -212,6 +241,186 @@ class ToolChain {
         let pathElem = addElement(this.masksContainer, "path", {d: this.calcPath()});
         this.pathElem = pathElem;
         for (let point of this.points) {
+            point.node.addEventListener("draggablepoint-move", (event) => {
+                setProperties(pathElem, {d: _this.calcPath()});
+            })
+        }
+    }
+
+    deactivate() {
+        this.isActive = false;
+        this.shells.root.setAttribute("visibility", "hidden");
+    }
+
+    activate() {
+        this.isActive = true;
+        this.shells.root.setAttribute("visibility", "visible");
+    }
+
+    delete() {
+        this.canvas.removeEventListener("mousedown", this.mousedownHandler);
+        if (this.pathElem) {this.masksContainer.removeChild(this.pathElem);}
+        this.canvas.removeChild(this.shells.root);
+    }
+}
+
+class ToolBezier {
+    constructor(id, toolCanvas, masksContainer) {
+        this.canvas = toolCanvas;
+        this.masksContainer = masksContainer;
+        this.id = id;
+
+        this.isActive = true;
+        this.r = 10;
+        this.cr = 6;
+        this.points = [];
+        this.controlPoints = [];
+
+        this.shells = {};
+        this.shells.root = addElement(this.canvas, "g", {id: `root_${this.id}`});
+        this.shells.paths = addElement(this.shells.root, "g", {id: `paths_${this.id}`});
+        this.shells.points = addElement(this.shells.root, "g", {id: `points_${this.id}`});
+
+        let _this = this;
+        this.mousedownHandler = (event) => {
+            if (event.target.tagName != "circle" && event.which == 1 && _this.isActive) {
+                _this.addPoint({
+                    x: event.pageX - getCoords(_this.canvas).x, 
+                    y: event.pageY - getCoords(_this.canvas).y
+                });
+            }
+        }
+        this.canvas.addEventListener("mousedown", this.mousedownHandler);
+    }
+
+    positionModifier() {
+        let _this = this;
+        return function(position) {
+            return {
+                x: Math.min(getSize(_this.canvas).width - _this.r, Math.max(_this.r, position.x)), 
+                y: Math.min(getSize(_this.canvas).height - _this.r, Math.max(_this.r, position.y))
+            }
+        }
+    }
+
+    createPoint(position, attrs, style) {
+        position = this.positionModifier()(position);
+        let point = new DraggablePoint(this.canvas, this.shells.points, position, attrs, style)
+        point.positionModifier = this.positionModifier();
+        return point;
+    }
+
+    createPath(point, prePoint) {
+        let position = point.position;
+        let prePosition = prePoint.position;
+        let ctrlPositin1 = {x: (prePosition.x * 2 + position.x) / 3,
+                            y: (prePosition.y * 2 + position.y) / 3};
+        let ctrlPositin2 = {x: (prePosition.x + position.x * 2) / 3,
+                            y: (prePosition.y + position.y * 2) / 3};
+        let ctrlPoint1 = this.createPoint(ctrlPositin1, {r: this.cr}, {fill: "grey"});
+        let ctrlPoint2 = this.createPoint(ctrlPositin2, {r: this.cr}, {fill: "grey"});
+        this.controlPoints.push(ctrlPoint1);
+        this.controlPoints.push(ctrlPoint2);
+        function pathStr(pp, cp1, cp2, p) {
+            return `M ${pp.x} ${pp.y} C ${cp1.x} ${cp1.y} ${cp2.x} ${cp2.y} ${p.x} ${p.y}`;
+        }
+        let path = addElement(this.shells.paths, "path", {
+            d: pathStr(prePosition, ctrlPositin1, ctrlPositin2, position),
+            stroke: "grey",
+            fill: "transparent"
+        })
+        function pMoveHandler() {
+            let p = point.startPointPosition;
+            let pp = prePoint.position;
+            let np = point.position;
+            let cp1 = ctrlPoint1._startPointPosition;
+            let cp2 = ctrlPoint2._startPointPosition;
+            let gomotet = complexDiv(complexSub(np, pp), complexSub(p, pp));
+            let ncp1 = complexAdd(pp, complexMul(complexSub(cp1, pp), gomotet));
+            let ncp2 = complexAdd(pp, complexMul(complexSub(cp2, pp), gomotet));
+            ctrlPoint1.moveTo(ncp1);
+            ctrlPoint2.moveTo(ncp2);
+            setProperties(path, {d: pathStr(prePoint.position, ctrlPoint1.position, 
+                ctrlPoint2.position, point.position)})
+        }
+        function ppMoveHandler() {
+            let p = point.position;
+            let pp = prePoint.startPointPosition;
+            let npp = prePoint.position;
+            let cp1 = ctrlPoint1._startPointPosition;
+            let cp2 = ctrlPoint2._startPointPosition;
+            let gomotet = complexDiv(complexSub(npp, p), complexSub(pp, p));
+            let ncp1 = complexAdd(p, complexMul(complexSub(cp1, p), gomotet));
+            let ncp2 = complexAdd(p, complexMul(complexSub(cp2, p), gomotet));
+            ctrlPoint1.moveTo(ncp1);
+            ctrlPoint2.moveTo(ncp2);
+            setProperties(path, {d: pathStr(prePoint.position, ctrlPoint1.position, 
+                ctrlPoint2.position, point.position)})
+        }
+        function ctrlMoveHandler() {
+            setProperties(path, {d: pathStr(prePoint.position, ctrlPoint1.position, 
+                ctrlPoint2.position, point.position)})
+        }
+        point.node.addEventListener("draggablepoint-move", pMoveHandler);
+        point.node.addEventListener("draggablepoint-dragstart", () => {
+            ctrlPoint1._startPointPosition = ctrlPoint1.position;
+            ctrlPoint2._startPointPosition = ctrlPoint2.position;
+        })
+        prePoint.node.addEventListener("draggablepoint-move", ppMoveHandler);
+        prePoint.node.addEventListener("draggablepoint-dragstart", () => {
+            ctrlPoint1._startPointPosition = ctrlPoint1.position;
+            ctrlPoint2._startPointPosition = ctrlPoint2.position;
+        })
+        ctrlPoint1.node.addEventListener("draggablepoint-move", ctrlMoveHandler);
+        ctrlPoint2.node.addEventListener("draggablepoint-move", ctrlMoveHandler);
+    }
+
+    addPoint(position) {
+        let point = this.createPoint(position, {r: this.r}, {fill: "grey"});
+        this.points.push(point);
+
+        if (this.points.length >= 2) {
+            let prePoint = this.points[this.points.length - 2];
+            this.createPath(point, prePoint);
+        } else {
+            let _this = this;
+            point.node.addEventListener("mousedown", (event) => {
+
+            // if (_this.points.length >= 3) 
+            // {
+            let point = _this.points[0];
+            let prePoint = _this.points[_this.points.length - 1];
+            _this.createPath(point, prePoint)
+            _this.canvas.removeEventListener("mousedown", _this.mousedownHandler);
+            _this.createMask();
+            _this.canvas.dispatchEvent(new CustomEvent("toolbezier-end", {detail: {id: _this.id}}));
+            // }
+
+            }, {once: true});
+        }
+    }
+
+    calcPath() {
+        let path = ""
+        for (let i = 0; i < this.points.length; i++) {
+            let cp1 = this.controlPoints[2 * i].position;
+            let cp2 = this.controlPoints[2 * i + 1].position;
+            let p = this.points[(i + 1) % this.points.length].position;
+            path += `C ${cp1.x} ${cp1.y} ${cp2.x} ${cp2.y} ${p.x} ${p.y}`
+        }
+        return `M ${this.points[0].position.x} ${this.points[0].position.y} ` + path;
+    }
+
+    createMask() {
+        let _this = this;
+        let pathElem = addElement(this.masksContainer, "path", {d: this.calcPath()});
+        this.pathElem = pathElem;
+        for (let point of this.points) {
+            point.node.addEventListener("draggablepoint-move", (event) => {
+                setProperties(pathElem, {d: _this.calcPath()});
+            })
+        }
+        for (let point of this.controlPoints) {
             point.node.addEventListener("draggablepoint-move", (event) => {
                 setProperties(pathElem, {d: _this.calcPath()});
             })
